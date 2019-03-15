dc70db151a025f21c8a47f16aeb284fd66da5893
root <root@ns300600.ip-91-121-65.eu>
2019-03-15 10:15:54 +0000

    update

--- KindleComicPusher/settings.py
+++ KindleComicPusher/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dm3t$e$&h5&+bty6j=q-y#u(5*zr&q-qjs27wt0y)-ji2qt&i!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['server.asairo.net','127.0.0.1','localhost','192.168.1.3' ]
 
